7b4f1e254fd8bcf0090b72861f564d20196b7775
Essie <49266945+ShinWooJeong@users.noreply.github.com>
2020-04-14 00:07:13 +0900

    Add files via upload

---
+++ day2_module.js
@@ -0,0 +1,23 @@
+////____ day2 ____////
+
+// //01_객체를 할당할 경우:   
+// var user = {
+//     getUser : function() {
+//         return { id: 'tt01', name: '양이'};
+//     },
+//     group : { id: 'tt02', name: '모찌'}
+// }
+// module.exports = user;
+
+
+
+// //02_함수를 할당할 경우:
+// module.exports = function() {
+//     return { id: 'tt03', name: '테리'};
+// };
+
+
+
+//04_exports 실험
+var gg = {id:'111', name:'petric'};
+exports = gg;
